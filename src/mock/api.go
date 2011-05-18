a95fbbaabcb77a3af31019632ec39f4a4d96baa0
Marko Mikulicic <marko.mikulicic@isti.cnr.it>
2011-05-18 18:00:53 +0200

    refacto mock api

--- src/mock/api.go
+++ src/mock/api.go
@@ -2,39 +2,20 @@ package main
 
 import (
 	"web"
-	"os"
 	"fmt"
 	"json"
+	"time"
+	"rand"
 )
-
-func Uuid() string {
-	f, _ := os.Open("/dev/urandom")
-	defer f.Close()
-	b := make([]byte, 16)
-	f.Read(b)
-	return fmt.Sprintf("%x-%x-%x-%x-%x", b[0:4], b[4:6], b[6:8], b[8:10], b[10:])
-}
 
 ////////////
 
-type JobRegistry struct {
-	submission chan JobRegistrySubmitRequest
-	listing chan JobRegistryListRequest
-}
-
-type JobRegistrySubmitRequest struct {
-	request JobRequest
-	response chan string
-}
-
-type JobRegistryListRequest struct {
-	response chan []Job
-}
 
 type JobStatus string
 
 const (
-	RUNNING JobStatus = "RUNNING"
+	QUEUED JobStatus = "WAITING"
+	RUNNING = "RUNNING"
 	DONE = "DONE"
 	FAILED = "FAILED"
 )
@@ -42,52 +23,45 @@ const (
 type Job struct {
 	Id string
 	Status JobStatus
+	Completion int
+	Metrics map[string]Metric
 }
 
-func NewJobRegistry() (this *JobRegistry) {
-	this = &JobRegistry{make(chan JobRegistrySubmitRequest, 10), make(chan JobRegistryListRequest, 10)}
+
+//
+func (this *Job) Run() {
+	fmt.Printf("Running job %s\n", this.Id)
+	this.Status = RUNNING
+
+	for {
+		time.Sleep(rand.Int63n(20) * 1e8)
+		
+		increment := rand.Intn(4)
+
+		comp := this.Completion + increment
+		if comp > 100 {
+			comp = 100
+		}
+		
+		this.Completion = comp
+
+		fmt.Printf("Progressing %s: %d%%\n", this.Id, this.Completion)
+		
+		if comp >= 100 {
+			this.Status = "DONE"
+			break
+		}
+	}
+}
+
+func Schedule(job *Job) {
+	fmt.Printf("Scheduling job %s\n", job.Id)
 
 	go func() {
-		jobMap := make(map[string] Job)
-		
-		for {
-			select {
-			case sub := <- this.submission:
-				job := Job{Uuid(), RUNNING}
-				jobMap[job.Id] = job
-				println("got request")
-				sub.response <- job.Id
-				
-			case list := <- this.listing:
-				
-				res := make([]Job, 0, 100)
-				for _, v := range jobMap {
-					res = append(res, v)
-				}
-				list.response <- res
-				
-			}
-		}
+		time.Sleep((10 + rand.Int63n(30)) * 1e8)
+		go job.Run()
 	}()
-
-	return
 }
-
-func (this *JobRegistry) Submit(job JobRequest) (string, os.Error) {
-	res := make(chan string, 1)
-	req := JobRegistrySubmitRequest{job, res}
-	this.submission <- req
-	return <-res, nil
-}
-
-func (this *JobRegistry) List() ([]Job, os.Error) {
-	res := make(chan []Job, 1)
-	req := JobRegistryListRequest{res}
-	this.listing <- req
-	return <-res, nil
-}
-
-
 
 ////////////
 
@@ -106,8 +80,15 @@ type SubmissionError struct {
 }
 
 type JobStatusResponse struct {
-	Count int "count"
-	Completed int "completed"
+	Id string "id"
+	Status JobStatus "status"
+	Completion int "completion"
+	Metrics map[string]Metric "metrics"
+}
+
+type Metric struct {
+	Name string "name"
+	
 }
 
 var registry JobRegistry = *NewJobRegistry()
@@ -119,32 +100,36 @@ func ApiSubmit(ctx *web.Context) string {
 
 	e := json.Unmarshal(ctx.Request.ParamData, &request)
 	if e != nil {
-		println("got error: ", e.String())
+		println("got unmarshal error: ", e.String())
 	}
 
 	println("request:", request.Test)
 
-	registry.Submit(request)
+	id, e := registry.Submit(request)
+	if e != nil {
+		println("got submit error: ", e.String())
+	}
 
 	ctx.ContentType("application/json")
-	job := &JobResponse{Uuid(), nil}
+	job := &JobResponse{id, nil}
 	m, _ := json.Marshal(job)
 	return string(m)
 }
 
 func getStatus(id string) *JobStatusResponse {
-	return &JobStatusResponse{10, 4}
+	job, e := registry.Get(id)
+	if e != nil {
+		println("got error", e.String())
+	}
+	return &JobStatusResponse{job.Id, job.Status, job.Completion, job.Metrics}
 }
 
 func ApiStatus(ctx *web.Context, id string) string {
 	ctx.ContentType("application/json")
 
 	status := getStatus(id)
-	count := status.Count
-	completed := status.Completed
 
-	res := &JobStatusResponse{count, completed}
-	m, _ := json.Marshal(res)
+	m, _ := json.Marshal(status)
 	return string(m)
 }
 
---
+++ src/mock/registry.go
@@ -0,0 +1,109 @@
+package main
+
+import (
+	"os"
+	"fmt"
+	cgl "tideland-cgl.googlecode.com/hg"
+)
+
+
+type JobRegistry struct {
+	submission chan JobRegistrySubmitRequest
+	listing chan JobRegistryListRequest
+	getting chan JobRegistryGetRequest
+}
+
+type JobRegistrySubmitRequest struct {
+	request JobRequest
+	response chan string
+	error chan os.Error
+}
+
+type JobRegistryListRequest struct {
+	response chan []Job
+	error chan os.Error
+}
+
+type JobRegistryGetRequest struct {
+	id string
+	response chan Job
+	error chan os.Error
+}
+
+
+func NewJobRegistry() (this *JobRegistry) {
+	this = &JobRegistry{make(chan JobRegistrySubmitRequest, 10), make(chan JobRegistryListRequest, 10), make(chan JobRegistryGetRequest, 10)}
+
+	go this.handler()
+
+	return
+}
+
+
+func (this *JobRegistry) handler() {
+	jobMap := make(map[string] *Job)
+	
+	for {
+		select {
+		case sub := <- this.submission:
+			job := Job{cgl.NewUUID().String(), QUEUED, 0, make(map[string]Metric)}
+			jobMap[job.Id] = &job
+			println("got submission")
+			
+			sub.response <- job.Id
+
+			Schedule(&job)
+			
+		case list := <- this.listing:			
+			res := make([]Job, 0, 100)
+			for _, v := range jobMap {
+				res = append(res, *v)
+			}
+			list.response <- res
+
+		case get := <- this.getting:
+			job, ok := jobMap[get.id]
+			if !ok {
+				get.error <- os.NewError("Cannot find job " + get.id)
+			} else {
+				get.response <- *job
+			}
+
+		}
+	}
+}
+
+
+func (this *JobRegistry) Submit(job JobRequest) (string, os.Error) {
+	res := make(chan string, 1)
+	error := make(chan os.Error, 1)
+
+	req := JobRegistrySubmitRequest{job, res, error}
+	this.submission <- req
+	return <-res, nil
+}
+
+func (this *JobRegistry) List() ([]Job, os.Error) {
+	res := make(chan []Job, 1)
+	error := make(chan os.Error, 1)
+
+	req := JobRegistryListRequest{res, error}
+	this.listing <- req
+	return <-res, nil
+}
+
+func (this *JobRegistry) Get(id string) (*Job, os.Error) {
+	res := make(chan Job, 1)
+	error := make(chan os.Error, 1)
+
+	req := JobRegistryGetRequest{id, res, error}
+	this.getting <- req
+	select {
+	case job := <- res:
+		fmt.Printf("GOT JOB %s\n", job.Id)
+		return &job, nil
+	case e := <- error:
+		return nil, e
+	}
+	return nil, nil
+}
