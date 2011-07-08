966cbaebd9ab2e7c092956d1a7769d50df66efb3
Marko Mikulicic <marko.mikulicic@isti.cnr.it>
2011-07-08 18:28:28 +0200

    fix return json

--- src/main/scala/cloud/SubmitterApi.scala
+++ src/main/scala/cloud/SubmitterApi.scala
@@ -50,7 +50,7 @@ class SubmitterApi extends ScalatraServlet with ScalateSupport with UrlSupport {
         """{"error" : "unknown job"}"""
       case Some(job) =>
         val status = if (job.completed) "DONE" else "RUNNING"
-        val metrics = if (job.completed) """""""" else buildMetrics(job)
+        val metrics = if (job.completed) "{}" else buildMetrics(job)
         val completion = (job.completedTasks: Double) * 100.0 / job.totalTasks
 
         """{"id":"%s","status":"%s","completion":%s,"metrics":%s}""".format(id, status, completion, metrics)
