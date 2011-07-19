72d1d6602e25ddd7136b6ef9ed88e49a6d41d1a8
Marko Mikulicic <marko.mikulicic@isti.cnr.it>
2011-07-19 21:07:19 +0200

    fix war startup

--- src/main/scala/GuiceWeb.scala
+++ src/main/scala/GuiceWeb.scala
@@ -1,18 +1,24 @@
 package it.cnr.aquamaps
+import it.cnr.aquamaps.cloud._
+
 import com.google.inject.Guice
 import com.google.inject.Injector
 import com.google.inject.servlet.GuiceServletContextListener
 import com.google.inject.servlet.ServletModule
 
-class WebServletModule extends ServletModule {
+import com.google.inject.util.{ Modules => GuiceModules }
+
+case class WebServletModule() extends ServletModule {
   override def configureServlets() = {
     serve("/submitter/*").`with`(classOf[ZeromqMonitoring])
     serve("/*").`with`(classOf[cloud.SubmitterApi])
+
   }
 }
 
 class GuiceServletConfig extends GuiceServletContextListener {
 
-  def getInjector(): Injector = Guice.createInjector(new WebServletModule())
+  def getInjector(): Injector = Guice.createInjector((GuiceModules `override` WebServletModule() `with` WebModule()))
+
 }
 
