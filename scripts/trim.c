706ffebe5bdd22e536fff250da1b1316372ad04c
Marko Mikulicic <marko.mikulicic@isti.cnr.it>
2012-02-24 11:29:34 +0100

    Updated trim

--- scripts/trim.c
+++ scripts/trim.c
@@ -1,7 +1,15 @@
+#include <sys/mman.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+#include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
 
+
 #define LINE_LENGTH 1024
+
 
 char line[LINE_LENGTH];
 
@@ -15,13 +23,7 @@ inline void truncate(char* l) {
 
     if(commas == 2) {
       l++;
-      /*
-        if(*l == '1') {
-        l++;
-        *l = 0;
-        return;
-      }
-      */
+
       while(skip-- && *l != ',')
         l++;
       *l = 0;
@@ -36,18 +38,73 @@ inline void truncate(char* l) {
   }
 }
 
+void process_lines(char* file, size_t size) {
+  while(1) {
+    const char* begin = file;
+
+    while(size-- && *file++)
+      if(*file == '\n')
+        break;
+
+    strncpy(line, begin, file-begin);
+    line[file-begin-1] = 0;
+
+    truncate(line);
+    puts(line);
+
+    size--;
+    if(!*file++)
+      break;
+  }
+}
+
 int main(int argc, char **argv) {
-  if(argc < 1) {
+  if(argc < 2) {
     printf("Missing input file name\n");
     exit(1);
   }
 
+  return mmap_main(argc, argv);
+}
+
+int mmap_main(int argc, char **argv) {
+  int fd = open(argv[1], 0);
+  if(fd == 0) {
+    printf("cannot open file %s\n", argv[1]);
+    exit(1);
+  }
+
+  struct stat stat;
+  fstat(fd, &stat);
+
+  size_t size = stat.st_size;
+
+  char* file = mmap(NULL, size, PROT_READ, MAP_SHARED|MAP_NORESERVE|MAP_POPULATE, fd, 0);
+
+  if(file == MAP_FAILED) {
+    perror("cannot mmap file");
+    exit(1);
+  }
+
+  process_lines(file, size);
+
+  munmap(file, size);
+  close(fd);
+  return 0;
+}
+
+
+int read_main(int argc, char **argv) {
   FILE *inFilePtr = fopen(argv[1], "r+");
+  if(inFilePtr == NULL) {
+    printf("cannot open file %s\n", argv[1]);
+    exit(1);
+  }
+
   while (!feof(inFilePtr)){
     fscanf(inFilePtr, "%s", line);
-    //    truncate(line);
-    //printf("%s\n", line);
-    //puts(line);
+    truncate(line);
+    puts(line);
   }
   return 0;
 }
